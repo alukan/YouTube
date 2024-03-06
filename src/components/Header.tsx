c20496f7e0a954e64ccfba1aa315b64f2aeb22dc
alukan <kolyaterenya@gmail.com>
2024-03-06 16:04:04 +0100

    added to full-end project

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,21 +4,37 @@ import { Link, useNavigate } from 'react-router-dom';
 import { StyledHeader, BackButton, StyledNav } from '../styles/HeaderStyles'
 
 const Header: React.FC = () => {
-    const navigate = useNavigate();
-  
-    return (
-      <StyledHeader>
-        <BackButton onClick={() => navigate(-1)}>
-          <FiArrowLeft size={24} />
-        </BackButton>
-        <StyledNav>
-          <Link to="/">
-            <FiHome size={24} />
-          </Link>
-        </StyledNav>
-      </StyledHeader>
-    );
+  const navigate = useNavigate();
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch('/api', {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
   };
-  
-  export default Header;
-  
+  return (
+    <StyledHeader>
+      <BackButton onClick={() => navigate(-1)}>
+        <FiArrowLeft size={24} />
+      </BackButton>
+      <StyledNav>
+        <Link to="/">
+          <FiHome size={24} />
+        </Link>
+        <button onClick={handleSubmit}>
+          <FiHome size={24} />
+        </button>
+      </StyledNav>
+    </StyledHeader>
+  );
+};
+
+export default Header;
+
